d960b7848fded64d3b34617121f95f7a3dfd8e08
tzar-beep <vaibhavkushwaha66@gmail.com>
2026-03-09 09:05:11 +0530

    Base application setup

---
+++ src/BookMyStay.java
@@ -0,0 +1,11 @@
+public class BookMyStay {
+    public static void main(String[] args) {
+
+        System.out.println("-------------------------------------");
+        System.out.println("Hotel Booking Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("Welcome!");
+        System.out.println("-------------------------------------");
+
+    }
+}
